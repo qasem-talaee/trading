323cb8dbd8c773a3ed1cdffc5a02cedc9ab3dc70
qasem talaee <qasem.talaee1375@gmail.com>
2021-05-23 20:52:29 +0430

    Add All coins

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 import requests
 import threading
 import time
+import os
 
 from libs import coin2
 
@@ -33,6 +34,40 @@ class Main(threading.Thread):
             if i.find('USDT') != -1:
                 self.list.append(i.replace('USDT', '').lower())
 
+    def check_log_file(self):
+        if not os.path.isdir('./logs'):
+            os.mkdir('./logs')
+        if not os.path.isfile('./logs/log_MAIN.txt'):
+            open('./logs/log_MAIN.txt', 'a').close()
+
+    def read_logger(self):
+        if os.stat('./logs/log_MAIN.txt').st_size != 0:
+            each_coin = self.money / self.n
+            with open('./logs/log_MAIN.txt', 'r') as logFile:
+                for line in logFile:
+                    r = coin2.Coin(line, each_coin, 0, 0, 1.5)
+                    r.start()
+                    r.join()
+                    self.runlist[self.j] = r
+                    self.j = self.j + 1
+                    l = ListenerCoin(self.runlist[self.j])
+                    l.start()
+                    l.join()
+
+    def logger(i):
+        with open('logs/log_MAIN.txt', 'a') as logFile:
+            logFile.write(i + '/n')
+
+    def delete_logger(i):
+        a_file = open("logs/log_MAIN.txt", "r")
+        lines = a_file.readlines()
+        a_file.close()
+        new_file = open("logs/log_MAIN.txt", "w")
+        for line in lines:
+            if line.strip("\n") != i:
+                new_file.write(line)
+        new_file.close()  
+
     def __init__(self, n, money):
         threading.Thread.__init__(self)
         self.list = []
@@ -42,6 +77,8 @@ class Main(threading.Thread):
         self.set_n(n)
         self.set_money(money)
         self.j = 0
+        self.check_log_file()
+        self.read_logger()
 
     def run(self):
         each_coin = self.money / self.n
@@ -52,19 +89,20 @@ class Main(threading.Thread):
                         print('start ' + i)
                         r = coin2.Coin(i, each_coin, 0, 0, 1.5)
                         r.start()
-                        time.sleep(10)
+                        time.sleep(20)
                         if r.flag_test:
                             print('buy ' + i)
-                            r.join()
                             self.runlist[self.j] = r
+                            r.join()
                             l = ListenerCoin(self.runlist[self.j])
                             l.start()
                             l.join()
                             self.j = self.j + 1
+                            self.logger(i)
                             continue
                         else:
                             print('del ' + i)
-                            del r
+                            r.__del__()
                             continue
 
 class ListenerCoin(threading.Thread):
@@ -77,7 +115,8 @@ class ListenerCoin(threading.Thread):
                 print('sell ' + obj)
                 Main().runlist.remove(obj)
                 Main().j -= 1
-                del obj
+                Main().delete_logger(obj.coin.lower())
+                obj.__del__()
                 flag = False
 
 #Main(2, 10).get_market_list()
